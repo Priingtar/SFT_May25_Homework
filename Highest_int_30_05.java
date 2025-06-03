e06cb5d8302e2412f77da4d16861e3c006cd4bbc
Priingtar <priyasinghkotar@gmail.com>
2025-06-03 09:36:56 +0530

    Add files via upload

--- Highest_int_30_05.java
+++ Highest_int_30_05.java
@@ -7,8 +7,9 @@ public class Highest_int_30_05 {
     int num1;
     int num2;
     int num3;
+    int res;
 
-    void Highest_finder()
+    void Highest_finder_and_optr()
     {
        if (num1 > num2 && num1 > num3)
        {
@@ -32,6 +33,21 @@ public class Highest_int_30_05 {
 
     }
 
+    void Highest_finder_ternory_optr_one(){
+        System.out.println("Using Second method ---------->>");
+        System.out.println((num1>num2 && num1>num3) ? num1 +" is highest" : (num2>num1 && num2>num3) ? num2 +" is highest" : num3+" is highest");
+
+    }
+
+    void Highest_finder_ternory_optr_two(){
+
+        //max = (num1 > num2) ? num1 : num2
+        // greatest = (max > num3) ? max: num3 ;
+        System.out.println("Using third method ---------->>");
+        res= (((num1 > num2) ? num1 : num2) > num3) ? ( (num1 > num2) ? num1 : num2 ) : num3 ;
+        System.out.println("The highest number is = "+ res);
+    }
+
     public static void main(String args[])
     {
        Scanner sc = new Scanner(System.in);
@@ -44,7 +60,10 @@ public class Highest_int_30_05 {
         System.out.println("Enter third number = ");
         hi.num3 = sc.nextInt();
 
-        hi.Highest_finder();
+        hi.Highest_finder_and_optr();
+        hi.Highest_finder_ternory_optr_one();
+        hi.Highest_finder_ternory_optr_two();
+
 
 
     }
